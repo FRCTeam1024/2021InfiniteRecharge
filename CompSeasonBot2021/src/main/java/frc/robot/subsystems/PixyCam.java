8e0ba2ff440a405ebdba991bf0d089a3d4da1bc8
qdtroemner <qdtroemner@outlook.com>
2021-02-13 16:03:49 -0500

    Add GetBlock Methods

--- CompSeasonBot2021/src/main/java/frc/robot/subsystems/PixyCam.java
+++ CompSeasonBot2021/src/main/java/frc/robot/subsystems/PixyCam.java
@@ -4,23 +4,54 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.ArrayList;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import io.github.pseudoresonance.pixy2api.Pixy2;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
 import io.github.pseudoresonance.pixy2api.links.Link;
 import io.github.pseudoresonance.pixy2api.links.SPILink;
 
 public class PixyCam extends SubsystemBase {
   private final Pixy2 pixy;
-  boolean isLampEnabled = false;
+  boolean isLampEnabled;
 
   /** Creates a new Pixy2. */
   public PixyCam() {
     pixy = Pixy2.createInstance(new SPILink());
-    pixy.init();
+    pixy.init(0); // Defaults to CS0, inputting 0 it just in case.
     pixy.setLamp((byte) 0, (byte) 0);
     this.isLampEnabled = false;
     pixy.setLED(0, 255, 0);
+  }
+
+  public int getBlockCount() {
+    return pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG2, 5);
+  }
+
+  public Block getFirstBlock() {
+    return pixy.getCCC().getBlockCache().get(0);
+  }
+
+  public Block getLargestBlock() {
+    int blockCount = pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG2, 5);
+    if(blockCount <= 0) {
+      return null;
+    }
+    ArrayList<Block> blocks = pixy.getCCC().getBlockCache();
+    Block largestBlock = null;
+    for (Block block : blocks) {
+      if(largestBlock == null) {
+        largestBlock = block;
+      } else if(block.getWidth() > largestBlock.getWidth()) {
+        largestBlock = block;
+      }
+    }
+
+    return largestBlock;
   }
 
   public void setLamp(int state) {
@@ -30,6 +61,12 @@ public class PixyCam extends SubsystemBase {
 
   @Override
   public void periodic() {
+    //SmartDashboard.putNumber("Total blocks", this.getBlockCount());
     // This method will be called once per scheduler run
+    /*Block currentBlock = this.getFirstBlock();
+    SmartDashboard.putNumber("Block x", currentBlock.getX());
+    SmartDashboard.putNumber("Block y", currentBlock.getY());
+    SmartDashboard.putNumber("Block width", currentBlock.getWidth());
+    SmartDashboard.putNumber("Block height", currentBlock.getHeight());*/
   }
 }
